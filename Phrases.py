ccfe0166bd1398d1313843a76167c3a8235ba94b
OlesiaVisich <visich.oi@phystech.edu>
2021-11-29 16:12:12 +0300

    'Second2'

--- Phrases.py
+++ Phrases.py
@@ -1,5 +1,4 @@
 def ReadReact (filename1, filename2, mas):
-    i=0
     with open(filename1, encoding='UTF8') as import_file1:
         with open(filename2, encoding='UTF8') as import_file2:
             for line in import_file1:
@@ -7,7 +6,12 @@ def ReadReact (filename1, filename2, mas):
                 st=import_file2.readline()
                 mas.append(st)
             
+
+def ReadQuest (filename, mas):
+    with open(filename, encoding='UTF8') as import_file:
+        for line in import_file:
+            mas.append(line)
+
 Test=[]
-ReadReact ('yes.txt', 'yesnum.txt', Test)
+ReadQuest ('yes.txt', Test)
 print (*Test, sep='\n')
-        
